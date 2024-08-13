fba596d4a3ea04702ce5ea28c2494f0d4ab2330c
AfsheenAfridi <167224884+AfsheenAfridi1@users.noreply.github.com>
2024-08-13 08:30:44 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let answers = await inquirer.prompt([
+    { message: "Enter First Number",type:"number",name: "firstNumber"},
+    { message: "Enter Second Number", type: "number", name: "secondNumber"},
+    {
+        message: "Select one Operator to perform action",
+        type: "List",
+        name: "operator",
+        Choices: ["Addition","Subtraction","Division","Multiplication"]
+    }
+]);
+
+//Conditional Statement
+if(answers.operator === "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber) ;
+}else if (answers.operator === "Subtraction"){
+    console.log(answers.firstNumber - answers.secondNumber) ;
+}else if(answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber) ;
+}else if(answers.operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber) ;
+}else{
+    console.log("please select a valid operator");
+    
+}
